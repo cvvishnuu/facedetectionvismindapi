0d53dc65b41cb0af8d5f2d1224afc37b228ecaa6
cvvishnuu <cvishnuu01@gmail.com>
2020-05-12 14:14:16 +0530

    cleaning up the code

---
+++ Controllers/signin.js
@@ -0,0 +1,21 @@
+const handleSignin = (req, res, db, bcrypt) => {
+	const { email, password } = req.body;
+	db.select('email', 'hash').from('login')
+		.where('email', '=', email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(password, data[0].hash);
+			if(isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('Unable to get the user'))
+			} else {
+				res.status(400).json('Wrong User Credentials')
+			}
+		})
+		.catch(err => res.status(400).json('Wrong User Credentials'))
+}
+
+module.exports = {handleSignin}
