bfd3ea377b38c906de4c37b267786249fc8a9520
cvvishnuu <cvishnuu01@gmail.com>
2020-05-12 23:00:32 +0530

    post assigned

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'aec7195bae514651bcc54fd03c082187'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})	
+		.catch(err => res.status(400).json('Unable to work with the API'))
+}
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)
@@ -9,4 +24,4 @@ const handleImage = (req, res, db) => {
 	.catch(err => res.status(400).json('unable to count the entries'))
 }
 
-module.exports = {handleImage}
+module.exports = {handleImage, handleApiCall}
--- server.js
+++ server.js
@@ -28,8 +28,10 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3001, () => {
-	console.log('app is running on port 3001');
+
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
